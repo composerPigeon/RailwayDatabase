081d0d8e6097cd3faef20c07cdd04dd249383d85
Josef Holubec <josefholubec@Josefs-MacBook-Air.local>
2023-08-14 14:37:08 +0200

    Integer data types updated and new table Drives created.

--- create_script.sql
+++ create_script.sql
@@ -1,51 +1,51 @@
 ---------PLACE_LOGIC TABLES----------
 
 create table Place(
-    id numeric
+    id integer
         constraint Place_PK primary key
 );
 
 create table Track(
-    id numeric
+    id integer
         constraint Track_id_PK primary key
         constraint Track_id_FK_Place
             references Place(id)
             on delete cascade,
     code character(6) not null
         constraint Track_code_Unique unique,
-    length numeric(5, 0)
+    length number(5, 0)
         constraint Track_length_gt0 check(length > 0),
-    speedLimit numeric(3, 0)
+    speedLimit number(3, 0)
         constraint Track_speedLimit_gt0 check(speedLimit > 0),
-    numOfRails numeric(1, 0) not null,
+    numOfRails number(1, 0) not null,
         constraint Track_numOfRails_gt0 check(numOfRails > 0)
 );
 
 create table Station(
-    id numeric 
+    id integer
         constraint Station_id_PK primary key
         constraint Station_id_FK_Place
             references Place(id)
             on delete cascade,
     name varchar2(50 char) not null
         constraint Station_name_Unique unique,
-    trainCapacity numeric not null
+    trainCapacity number not null
         constraint Station_trainCap_gt0 check(trainCapacity > 0),
-    cargoType varchar2(50) default "passengers" not null,
-    capacity numeric
-        constraint Carriage_cap_gt0 check(capacity > 0),
-    capacityUnit varchar2(10) default "passanger" not null
+    cargoType varchar2(50) default('passengers') not null,
+    cargoCapacity number
+        constraint Carriage_cap_gt0 check(cargoCapacity > 0),
+    capacityUnit varchar2(10) default('p.') not null
 );
 
 ---------TRAIN_LOGIC TABLES----------
 
 --needs trigger when place is deleted (dont want to delete trains at a station)
 create table Train(
-    id numeric
+    id integer
         constraint Train_id_PK primary key,
     name varchar2(50 char) not null,
         constraint Train_name_Unique unique,
-    placeId numeric not null
+    placeId integer not null
         constraint Train_placeId_FK_Place
             references Place(id)
 );
@@ -53,36 +53,36 @@ create table Train(
 create index Train_placeId_Inx on Train(placeId);
 
 create table Car (
-    id numeric
+    id integer
         constraint Car_id_PK primary key,
     brand varchar2(50) not null,
     model varchar2(50) not null,
-    maxSpeed numeric
+    maxSpeed number
         constraint Car_maxSpeed_gt0 check(maxSpeed > 0),
-    maxWeight numeric
+    maxWeight number
         constraint Car_maxWeight_gt0 check(maxWeight > 0)
 );
 
 create table Carriage (
-    id numeric
+    id integer
         constraint Carriage_id_PK primary key
         constraint Carriage_id_FK_Car
             references Car(id)
             on delete cascade,
-    cargoType varchar2(50) default "passangers" not null,
-    capacity numeric
+    cargoType varchar2(50) default('passangers') not null,
+    capacity number
         constraint Carriage_cap_gt0 check(capacity > 0),
-    capacityUnit varchar2(10) "passanger" not null
+    capacityUnit varchar2(10) default('p.') not null
 );
 
 create table License (
-    id numeric
+    id integer
         constraint License_id_PK primary key,
     description varchar2(50 char)
 );
 
 create table Locomotive (
-    id numeric
+    id integer
         constraint Locomotive_id_PK primary key
         constraint Locomotive_id_FK_Car
             references Car(id)
@@ -97,11 +97,11 @@ create table Locomotive (
 create index Locomotive_licenseId_inx on Locomotive(licenseId);
 
 create table TrainRecipe (
-    trainId numeric
+    trainId integer
         constraint TrainRecipe_trainId_FK_Train
             references Train(id)
             on delete cascade,
-    carId numeric
+    carId integer
         constraint TrainRecipe_carId_FK_Car
             references Car(id)
             on delete cascade,
@@ -115,16 +115,16 @@ create index TrainRecipe_carId_Inx on TrainRecipe(carId);
 -------EMPLOYEE_LOGIC TABLES--------
 
 create table Employee (
-    id numeric
+    id integer
         constraint Employee_id_PK primary key,
     name varchar2(50) not null,
     email varchar2(50) not null
         constraint Employee_email_Unique unique,
-    phone numeric not null
+    phone number(9, 0) not null
 );
 
 create table TrainDriver (
-    id numeric
+    id integer
         constraint TrainDriver_id_PK primary key
         constraint TrainDriver_id_FK_Employee
             references Employee(id)
@@ -132,11 +132,11 @@ create table TrainDriver (
 );
 
 create table HasLicense (
-    licenseId numeric
+    licenseId integer
         constraint HasLicense_licenseId_FK_License
             references License(id)
             on delete cascade,
-    driverId numeric
+    driverId integer
         constraint HasLicense_driverId_FK_TrainDriver
             references TrainDriver(id)
             on delete cascade,
@@ -145,3 +145,15 @@ create table HasLicense (
 
 create index HasLicense_licenseId_Inx on HasLicense(licenseId);
 create index HasLicense_driverId_Inx on HasLicense(driverId);
+
+create table Drives (
+    driverId integer
+        constraint Drives_driverId_FK_TrainDriver
+            references TrainDriver(id)
+            on delete cascade,
+    trainId integer
+        constraint Drives_trainId_FK_Train
+            references Train(id)
+            on delete cascade,
+    constraint Drives_PK primary key (driverId, trainId)
+);
