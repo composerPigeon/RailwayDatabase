6bdb92457f71a6cd148347d035928d781568fe52
Josef Holubec <josefholubec@Josefs-MacBook-Air.local>
2023-08-15 23:19:17 +0200

    Delete script from all tables and place logic added.

---
+++ delete_script.sql
@@ -0,0 +1,30 @@
+
+----DROP TRIGGERS----
+drop Station_Del_Trigger;
+drop Station_Ins_Trigger;
+drop Track_Del_Trigger;
+drop Track_Ins_Trigger;
+drop Place_Ins_Upd_Trigger;
+drop Place_Del_Trigger;
+
+----DROP SEQUENCES----
+drop sequence Seq_placeId;
+
+----DROP PKGs----
+drop package PlaceUI;
+
+----DROP TABLES----
+drop table Track;
+drop table Station;
+drop table TrainRecipe;
+drop table Train;
+drop table Place;
+drop table Carriage;
+drop table Drives;
+drop table Locomotive;
+drop table Car;
+drop table HasLicense;
+drop table License;
+drop table TrainDriver;
+drop table Employee;
+
